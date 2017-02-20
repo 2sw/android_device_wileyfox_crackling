29a05310ad94c553c1a9772fc7ab74f58d13a668
German Kirsanov <germankirsanov2011@yandex.ru>
2017-02-20 18:09:18 +0500

    fix twrp

--- product/twrp.mk
+++ product/twrp.mk
@@ -1,2 +1,2 @@
 PRODUCT_COPY_FILES += \
-    device/google/seed/recovery/root/etc/twrp.fstab:recovery/root/etc/twrp.fstab
+    device/wileyfox/crackling/recovery/root/etc/twrp.fstab:recovery/root/etc/twrp.fstab
