257ce734a0c22652d3c107e13c04eed5364b3833
German Kirsanov <germankirsanov2011@yandex.ru>
2017-02-20 16:33:00 +0500

    fix toolchain

--- board/kernel.mk
+++ board/kernel.mk
@@ -3,5 +3,5 @@ BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
 TARGET_KERNEL_SOURCE := kernel/wilefox/crackling
 TARGET_KERNEL_CONFIG := lineageos_crackling_defconfig
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9-google/prebuilt/linux-x86_64/bin/
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
